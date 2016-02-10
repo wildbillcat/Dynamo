306147ddbf8a82b36dd124210ae6b4df9a6facd3
Patrick McMorran <kingrolloinc@sbcglobal.net>
2016-02-10 12:40:05 -0500

    used document model

--- Dynamo/Program.cs
+++ Dynamo/Program.cs
@@ -17,49 +17,85 @@ namespace Dynamo
             var config = new AmazonDynamoDBConfig();
             config.ServiceURL = "http://localhost:8000";
             AmazonDynamoDBClient client = new AmazonDynamoDBClient(config);
-            string tableName = "ProductCatalog";
+            //string tableName = "ProductCatalog";
 
-            var request = new CreateTableRequest
+            //var request = new CreateTableRequest
+            //{
+            //    TableName = tableName,
+            //    AttributeDefinitions = new List<AttributeDefinition>()
+            //    {
+            //        new AttributeDefinition
+            //        {
+            //            AttributeName = "Id",
+            //            AttributeType = "N"
+            //        }
+            //    },
+            //            KeySchema = new List<KeySchemaElement>()
+            //    {
+            //        new KeySchemaElement
+            //        {
+            //            AttributeName = "Id",
+            //            KeyType = "HASH"  //Partition key
+            //        }
+            //        },
+            //    ProvisionedThroughput = new ProvisionedThroughput
+            //    {
+            //        ReadCapacityUnits = 10,
+            //        WriteCapacityUnits = 5
+            //    }
+            //};
+
+            //CreateTableResponse response = client.CreateTable(request);
+
+            //var tableDescription = response.TableDescription;
+            //Console.WriteLine("{1}: {0} \t ReadCapacityUnits: {2} \t WriteCapacityUnits: {3}",
+            //                tableDescription.TableStatus,
+            //                tableDescription.TableName,
+            //                tableDescription.ProvisionedThroughput.ReadCapacityUnits,
+            //                tableDescription.ProvisionedThroughput.WriteCapacityUnits);
+
+            //string status = tableDescription.TableStatus;
+            //Console.WriteLine(tableName + " - " + status);
+
+            //var res = client.DescribeTable(new DescribeTableRequest { TableName = "ProductCatalog" });
+            Console.WriteLine("Test Crud?");
+            Console.ReadLine();
+
+            DynamoDBContext context = new DynamoDBContext(client);
+            TestCRUDOperations(context);
+            Console.ReadLine();
+        }
+
+        private static void TestCRUDOperations(DynamoDBContext context)
+        {
+            int bookID = 1001; // Some unique value.
+            Book myBook = new Book
             {
-                TableName = tableName,
-                AttributeDefinitions = new List<AttributeDefinition>()
-                {
-                    new AttributeDefinition
-                    {
-                        AttributeName = "Id",
-                        AttributeType = "N"
-                    }
-                },
-                        KeySchema = new List<KeySchemaElement>()
-                {
-                    new KeySchemaElement
-                    {
-                        AttributeName = "Id",
-                        KeyType = "HASH"  //Partition key
-                    }
-                    },
-                ProvisionedThroughput = new ProvisionedThroughput
-                {
-                    ReadCapacityUnits = 10,
-                    WriteCapacityUnits = 5
-                }
+                Id = bookID,
+                Title = "object persistence-AWS SDK for.NET SDK-Book 1001",
+                ISBN = "111-1111111001",
+                BookAuthors = new List<string> { "Author 1", "Author 2" },
             };
 
-            CreateTableResponse response = client.CreateTable(request);
-            
-            var tableDescription = response.TableDescription;
-            Console.WriteLine("{1}: {0} \t ReadCapacityUnits: {2} \t WriteCapacityUnits: {3}",
-                            tableDescription.TableStatus,
-                            tableDescription.TableName,
-                            tableDescription.ProvisionedThroughput.ReadCapacityUnits,
-                            tableDescription.ProvisionedThroughput.WriteCapacityUnits);
+            // Save the book.
+            context.Save(myBook);
+            // Retrieve the book. 
+            Book bookRetrieved = context.Load<Book>(bookID);
 
-            string status = tableDescription.TableStatus;
-            Console.WriteLine(tableName + " - " + status);
+            // Update few properties.
+            bookRetrieved.ISBN = "222-2222221001";
+            bookRetrieved.BookAuthors = new List<string> { " Author 1", "Author x" }; // Replace existing authors list with this.
+            context.Save(bookRetrieved);
 
-            var res = client.DescribeTable(new DescribeTableRequest { TableName = "ProductCatalog" });
+            // Retrieve the updated book. This time add the optional ConsistentRead parameter using DynamoDBContextConfig object.
+            Book updatedBook = context.Load<Book>(bookID, new DynamoDBContextConfig { ConsistentRead = true });
 
-            Console.ReadLine();
+            // Delete the book.
+            context.Delete<Book>(bookID);
+            // Try to retrieve deleted book. It should return null.
+            Book deletedBook = context.Load<Book>(bookID, new DynamoDBContextConfig { ConsistentRead = true });
+            if (deletedBook == null)
+                Console.WriteLine("Book is deleted");
 
         }
     }

ab821403f66cd2f23a5d7b8b8fcb9d37370b9ae3
Patrick McMorran <kingrolloinc@sbcglobal.net>
2016-02-10 23:13:44 -0500

    updated sku

--- Dynamo/SKU.cs
+++ Dynamo/SKU.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2.DataModel;
 
 namespace Dynamo
 {
+    [DynamoDBTable("SKUs")]
     class SKU
     {
+        [DynamoDBHashKey]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        
+        public Dictionary<DateTime, int> MonthlyTotals { get; set; }        
     }
 }
